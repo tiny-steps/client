1cdfea1a9332a960ae48c5b65200a9aad2c801b5
subh-m23 <subh.m@sascoders.com>
2025-08-14 15:44:42 +0530

    refactor: remove logging and unused proxy response handling in Vite config

--- vite.config.js
+++ vite.config.js
@@ -23,37 +23,12 @@ export default defineConfig({
         cookieDomainRewrite: "localhost",
         configure: (proxy, options) => {
           proxy.on("proxyReq", (proxyReq, req, res) => {
-            console.log("Proxy request:", req.method, req.url);
-            console.log("Request cookies:", req.headers.cookie);
             // Keep original cookies instead of overriding origin
             if (req.headers.cookie) {
               proxyReq.setHeader("Cookie", req.headers.cookie);
             }
           });
-          proxy.on("proxyRes", (proxyRes, req, res) => {
-            console.log("Proxy response:", proxyRes.statusCode, req.url);
-            console.log("Response headers:", proxyRes.headers);
-
-            // Fix Set-Cookie headers for local development
-            // if (proxyRes.headers["set-cookie"]) {
-            //   proxyRes.headers["set-cookie"] = proxyRes.headers[
-            //     "set-cookie"
-            //   ].map((cookie) => {
-            //     // Remove Secure flag for local development (HTTP)
-            //     return cookie.replace(/;\s*Secure/gi, "");
-            //   });
-            //   console.log("Modified cookies:", proxyRes.headers["set-cookie"]);
-            // }
-
-            // Add CORS headers to the response
-            // proxyRes.headers["access-control-allow-origin"] =
-            //   "http://localhost:5174";
-            // proxyRes.headers["access-control-allow-credentials"] = "true";
-            // proxyRes.headers["access-control-allow-methods"] =
-            //   "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS";
-            // proxyRes.headers["access-control-allow-headers"] =
-            //   "Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie";
-          });
+          proxy.on("proxyRes", (proxyRes, req, res) => {});
         },
       },
     },
